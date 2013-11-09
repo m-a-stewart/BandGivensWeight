6bed33437aa48d5ba87f04ce5639703a97e4390d
mastewart <mastewart@gsu.edu>
2013-11-09 21:46:46 +0000

    Upper BV to UB added.

--- src/upper_convert.f90
+++ src/upper_convert.f90
@@ -217,4 +217,216 @@ contains
     end do
   end subroutine c_upper_ub_to_bv
 
+  subroutine d_upper_bv_to_ub(b_bv, nb_bv, n, lbw, ubw, numrots_bv, k1s_bv, k2s_bv, cs_bv, ss_bv, &
+       b_ub, mb_ub, numrots_ub, j1s_ub, j2s_ub, cs_ub, ss_ub, error)
+    real(kind=dp), dimension(n,nb_bv), intent(inout) :: b_bv
+    integer(kind=int32), intent(in) :: mb_ub, n, lbw, ubw, nb_bv
+    integer(kind=int32), dimension(n), intent(in) :: numrots_bv
+    integer(kind=int32), dimension(n,nb_bv-lbw-1), intent(in) :: k1s_bv, k2s_bv
+    real(kind=dp), dimension(n,nb_bv-lbw-1), intent(in) :: cs_bv, ss_bv
+
+    real(kind=dp), dimension(mb_ub,n), intent(out) :: b_ub
+    integer(kind=int32), dimension(n), intent(out) :: numrots_ub
+    integer(kind=int32), dimension(mb_ub-lbw-1,n), intent(out) :: j1s_ub, j2s_ub
+    real(kind=dp), dimension(mb_ub-lbw-1,n), intent(out) :: cs_ub, ss_ub
+    integer(kind=int32), intent(out) :: error
+
+    integer(kind=int32) :: j, k, l, roffs, coffs, ubw1, lbw1, d
+    type(d_rotation) :: rot
+
+    error = 0
+    b_ub=0.0_dp; numrots_ub=0
+    ss_ub=0.0_dp; cs_ub=0.0_dp
+    j1s_ub=0; j2s_ub=0
+    ubw1=ubw+1; lbw1=lbw
+    l = 1 ! The last row of b_bv is already revealed in b_ub
+    
+    if (n < 1) then
+       error = 1
+       return
+    end if
+    if (n == 1) then
+       b_ub(1,1)=b_bv(1,1)
+       return
+    end if
+    ! must allow for temporary fill-in
+    if (nb_bv<ubw1+lbw1+1) then
+       error = 2
+       return
+    end if
+    b_bv(:,ubw1+lbw1+1)=0.0_dp
+    ! apply v_{n-1}
+    do j=1, numrots_bv(n-1)
+       rot%cosine=cs_bv(n-1,j); rot%sine=ss_bv(n-1,j)
+       call tbr_times_rotation(b_bv,n,lbw1,ubw1,n-1,trp_rot(rot),k1s_bv(n-1,j))
+    end do
+    ! Generate a (ubw+1) x ubw lower triangular matrix.
+    do k=2,ubw1-1
+       coffs=ubw1
+       numrots_ub(k)=k-1
+       do j=k-1,1,-1
+          rot=lgivens2(get_el_br(b_bv,lbw1,j,coffs+j), get_el_br(b_bv,lbw1,j+1,coffs+j))
+          cs_ub(j,k)=rot%cosine; ss_ub(j,k)=rot%sine
+          j1s_ub(j,k)=j; j2s_ub(j,k)=j+1
+          call rotation_times_tbr(trp_rot(rot),b_bv,n,lbw1,ubw1,k,j)
+          call set_el_br(b_bv,lbw1,j,coffs+j, 0.0_dp)
+       end do
+       do j=1,numrots_bv(n-k)
+          rot%cosine=cs_bv(n-k,j); rot%sine=ss_bv(n-k,j)
+          call tbr_times_rotation(b_bv,n,lbw1,ubw1,n-k,trp_rot(rot),k1s_bv(n-k,j))
+       end do
+    end do
+    do k=ubw1,n-ubw1
+       roffs=k-ubw1; coffs=k
+       numrots_ub(k)=ubw1-1
+       do j=ubw1-1,1,-1
+          rot=lgivens2(get_el_br(b_bv,lbw1,roffs+j,coffs+j), get_el_br(b_bv,lbw1,roffs+j+1,coffs+j))
+          cs_ub(j,k)=rot%cosine; ss_ub(j,k)=rot%sine
+          j1s_ub(j,k)=roffs+j; j2s_ub(j,k)=roffs+j+1
+          call rotation_times_tbr(trp_rot(rot), b_bv,n,lbw1,ubw1, k, roffs+j)
+          call set_el_br(b_bv,lbw1,roffs+j,coffs+j, 0.0_dp)
+       end do
+       do j=1,numrots_bv(n-k)
+          rot%cosine=cs_bv(n-k,j); rot%sine=ss_bv(n-k,j)
+          call tbr_times_rotation(b_bv,n,lbw1,ubw1,n-k,trp_rot(rot),k1s_bv(n-k,j))
+       end do
+    end do
+    if (n-ubw>ubw) then ! if n-ubw=ubw, there is a square upper triangular matrix already.
+       do k=n-ubw,n-1
+          numrots_ub(k)=n-k
+          coffs=k
+          roffs=k-ubw1
+          do j=n-k,1,-1
+             rot=lgivens2(get_el_br(b_bv,lbw1,roffs+j,coffs+j),get_el_br(b_bv,lbw1,roffs+j+1,coffs+j))
+             cs_ub(j,k)=rot%cosine; ss_ub(j,k)=rot%sine
+             j1s_ub(j,k)=roffs+j; j2s_ub(j,k)=roffs+j+1
+             call rotation_times_tbr(trp_rot(rot), b_bv,n,lbw1,ubw1, k, roffs+j)
+             call set_el_br(b_bv,lbw1,roffs+j,coffs+j, 0.0_dp)
+          end do
+          do j=1,numrots_bv(n-k)
+             rot%cosine=cs_bv(n-k,j); rot%sine=ss_bv(n-k,j)
+             call tbr_times_rotation(b_bv,n,lbw1,ubw1,n-k,trp_rot(rot),k1s_bv(n-k,j))
+          end do
+       end do
+    end if
+    ! store the results in b_ub
+    do d=1,lbw+1
+       do j=1,n-lbw+d-1
+          b_ub(ubw+lbw+2-d,j)=b_bv(lbw-d+j+1,d)
+       end do
+    end do
+    do d=lbw+2,lbw+ubw+1
+       do j=1,n-d+lbw+1
+          b_ub(ubw+lbw+2-d,j+d-lbw-1)=b_bv(j,d)
+       end do
+    end do
+  end subroutine d_upper_bv_to_ub
+
+  subroutine c_upper_bv_to_ub(b_bv, nb_bv, n, lbw, ubw, numrots_bv, k1s_bv, k2s_bv, cs_bv, ss_bv, &
+       b_ub, mb_ub, numrots_ub, j1s_ub, j2s_ub, cs_ub, ss_ub, error)
+    complex(kind=dp), dimension(n,nb_bv), intent(inout) :: b_bv
+    integer(kind=int32), intent(in) :: mb_ub, n, lbw, ubw, nb_bv
+    integer(kind=int32), dimension(n), intent(in) :: numrots_bv
+    integer(kind=int32), dimension(n,nb_bv-lbw-1), intent(in) :: k1s_bv, k2s_bv
+    complex(kind=dp), dimension(n,nb_bv-lbw-1), intent(in) :: cs_bv, ss_bv
+
+    complex(kind=dp), dimension(mb_ub,n), intent(out) :: b_ub
+    integer(kind=int32), dimension(n), intent(out) :: numrots_ub
+    integer(kind=int32), dimension(mb_ub-lbw-1,n), intent(out) :: j1s_ub, j2s_ub
+    complex(kind=dp), dimension(mb_ub-lbw-1,n), intent(out) :: cs_ub, ss_ub
+    integer(kind=int32), intent(out) :: error
+
+    integer(kind=int32) :: j, k, l, roffs, coffs, ubw1, lbw1, d
+    type(c_rotation) :: rot
+
+    error = 0
+    b_ub=(0.0_dp, 0.0_dp); numrots_ub=0
+    ss_ub=(0.0_dp, 0.0_dp); cs_ub=(0.0_dp, 0.0_dp)
+    j1s_ub=0; j2s_ub=0
+    ubw1=ubw+1; lbw1=lbw
+    l = 1 ! The last row of b_bv is already revealed in b_ub
+    
+    if (n < 1) then
+       error = 1
+       return
+    end if
+    if (n == 1) then
+       b_ub(1,1)=b_bv(1,1)
+       return
+    end if
+    ! must allow for temporary fill-in
+    if (nb_bv<ubw1+lbw1+1) then
+       error = 2
+       return
+    end if
+    b_bv(:,ubw1+lbw1+1)=(0.0_dp, 0.0_dp)
+    ! apply v_{n-1}
+    do j=1, numrots_bv(n-1)
+       rot%cosine=cs_bv(n-1,j); rot%sine=ss_bv(n-1,j)
+       call tbr_times_rotation(b_bv,n,lbw1,ubw1,n-1,trp_rot(rot),k1s_bv(n-1,j))
+    end do
+    ! Generate a (ubw+1) x ubw lower triangular matrix.
+    do k=2,ubw1-1
+       coffs=ubw1
+       numrots_ub(k)=k-1
+       do j=k-1,1,-1
+          rot=lgivens2(get_el_br(b_bv,lbw1,j,coffs+j), get_el_br(b_bv,lbw1,j+1,coffs+j))
+          cs_ub(j,k)=rot%cosine; ss_ub(j,k)=rot%sine
+          j1s_ub(j,k)=j; j2s_ub(j,k)=j+1
+          call rotation_times_tbr(trp_rot(rot),b_bv,n,lbw1,ubw1,k,j)
+          call set_el_br(b_bv,lbw1,j,coffs+j, (0.0_dp, 0.0_dp))
+       end do
+       do j=1,numrots_bv(n-k)
+          rot%cosine=cs_bv(n-k,j); rot%sine=ss_bv(n-k,j)
+          call tbr_times_rotation(b_bv,n,lbw1,ubw1,n-k,trp_rot(rot),k1s_bv(n-k,j))
+       end do
+    end do
+    do k=ubw1,n-ubw1
+       roffs=k-ubw1; coffs=k
+       numrots_ub(k)=ubw1-1
+       do j=ubw1-1,1,-1
+          rot=lgivens2(get_el_br(b_bv,lbw1,roffs+j,coffs+j), get_el_br(b_bv,lbw1,roffs+j+1,coffs+j))
+          cs_ub(j,k)=rot%cosine; ss_ub(j,k)=rot%sine
+          j1s_ub(j,k)=roffs+j; j2s_ub(j,k)=roffs+j+1
+          call rotation_times_tbr(trp_rot(rot), b_bv,n,lbw1,ubw1, k, roffs+j)
+          call set_el_br(b_bv,lbw1,roffs+j,coffs+j, (0.0_dp, 0.0_dp))
+       end do
+       do j=1,numrots_bv(n-k)
+          rot%cosine=cs_bv(n-k,j); rot%sine=ss_bv(n-k,j)
+          call tbr_times_rotation(b_bv,n,lbw1,ubw1,n-k,trp_rot(rot),k1s_bv(n-k,j))
+       end do
+    end do
+    if (n-ubw>ubw) then ! if n-ubw=ubw, there is a square upper triangular matrix already.
+       do k=n-ubw,n-1
+          numrots_ub(k)=n-k
+          coffs=k
+          roffs=k-ubw1
+          do j=n-k,1,-1
+             rot=lgivens2(get_el_br(b_bv,lbw1,roffs+j,coffs+j),get_el_br(b_bv,lbw1,roffs+j+1,coffs+j))
+             cs_ub(j,k)=rot%cosine; ss_ub(j,k)=rot%sine
+             j1s_ub(j,k)=roffs+j; j2s_ub(j,k)=roffs+j+1
+             call rotation_times_tbr(trp_rot(rot), b_bv,n,lbw1,ubw1, k, roffs+j)
+             call set_el_br(b_bv,lbw1,roffs+j,coffs+j, (0.0_dp, 0.0_dp))
+          end do
+          do j=1,numrots_bv(n-k)
+             rot%cosine=cs_bv(n-k,j); rot%sine=ss_bv(n-k,j)
+             call tbr_times_rotation(b_bv,n,lbw1,ubw1,n-k,trp_rot(rot),k1s_bv(n-k,j))
+          end do
+       end do
+    end if
+    ! store the results in b_ub
+    do d=1,lbw+1
+       do j=1,n-lbw+d-1
+          b_ub(ubw+lbw+2-d,j)=b_bv(lbw-d+j+1,d)
+       end do
+    end do
+    do d=lbw+2,lbw+ubw+1
+       do j=1,n-d+lbw+1
+          b_ub(ubw+lbw+2-d,j+d-lbw-1)=b_bv(j,d)
+       end do
+    end do
+  end subroutine c_upper_bv_to_ub
+
+
+
 end module upper_convert
--- test/test_convert.f90
+++ test/test_convert.f90
@@ -3,8 +3,9 @@ program test_convert
   use utility
   use assemble
   use upper_convert
+  use band
   implicit none
-  integer(kind=int32), parameter :: n=1000, rmax=2, ubwmax=rmax, lbw=4, mb=ubwmax+lbw+3, nb=mb
+  integer(kind=int32), parameter :: n=500, rmax=4, ubwmax=rmax, lbw=3, mb=ubwmax+lbw+5, nb=mb
   real(kind=dp), parameter :: tol=1e-15
   real(kind=dp) :: t1, t2
   integer(kind=int32) :: j, k, error
@@ -70,6 +71,28 @@ program test_convert
      end if
   end if
 
+
+
+  ! bv to ub
+  a=a0
+  call upper_general_to_upper_bv(a,n, b_bv, mb, lbw, ubw, numrots_bv, k1s, k2s,  &
+       cs_bv, ss_bv, tol, error)
+  if (error == 1) then
+     print *, "orthogonalization error in real BV"
+  else
+     call cpu_time(t1)
+     call d_upper_bv_to_ub(b_bv, mb, n, lbw, ubw, numrots_bv, k1s, k2s, cs_bv, ss_bv, &
+          b_ub, mb, numrots_ub, j1s, j2s, cs_ub, ss_ub, error)
+     call cpu_time(t2)
+     if (error > 0) then
+        print *, "Error in upper_bv_to_ub:", error
+     else
+        call form_upper_ub(a1, n, b_ub, mb, lbw, ubw, numrots_ub, j1s, j2s, cs_ub, ss_ub)
+        write (*, "('Real BV to UB;                     ', 'Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
+             t2-t1, ubw, maxabs(a1-a0)/maxabs(a0)
+     end if
+  end if
+
 ! complex
 
   call random_complex(u_c)
@@ -85,6 +108,7 @@ program test_convert
   do j=1,n
      a_c(j,j)=d_c(j)
   end do
+  ! ub to bv
   a0_c=a_c
   call upper_general_to_upper_ub(a_c,n, b_ub_c, mb, lbw, ubw_c, numrots_ub_c, j1s_c, j2s_c,  &
        cs_ub_c, ss_ub_c, tol, error)
@@ -99,8 +123,27 @@ program test_convert
         print *, "Error in upper_ub_to_bv:", error
      else
         call form_upper_bv(a1_c, n, b_bv_c, mb, lbw, ubw_c, numrots_bv_c, k1s_c, k2s_c, cs_bv_c, ss_bv_c)
-        write (*, "('Real UB to BV;                     ', 'Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
-             t2-t1, ubw_c, maxabs(a1_c-a0_c)
+        write (*, "('Complex UB to BV;                     ', 'Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
+             t2-t1, ubw_c, maxabs(a1_c-a0_c)/maxabs(a0_c)
+     end if
+  end if
+  ! bv to ub
+  a_c=a0_c
+  call upper_general_to_upper_bv(a_c,n, b_bv_c, mb, lbw, ubw_c, numrots_bv_c, k1s_c, k2s_c,  &
+       cs_bv_c, ss_bv_c, tol, error)
+  if (error == 1) then
+     print *, "orthogonalization error in complex BV"
+  else
+     call cpu_time(t1)
+     call c_upper_bv_to_ub(b_bv_c, mb, n, lbw, ubw_c, numrots_bv_c, k1s_c, k2s_c, cs_bv_c, ss_bv_c, &
+          b_ub_c, mb, numrots_ub_c, j1s_c, j2s_c, cs_ub_c, ss_ub_c, error)
+     call cpu_time(t2)
+     if (error > 0) then
+        print *, "Error in upper_bv_to_ub:", error
+     else
+        call form_upper_ub(a1_c, n, b_ub_c, mb, lbw, ubw_c, numrots_ub_c, j1s_c, j2s_c, cs_ub_c, ss_ub_c)
+        write (*, "('Complex BV to UB;                     ', 'Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
+             t2-t1, ubw_c, maxabs(a1_c-a0_c)/maxabs(a0_c)
      end if
   end if
 
