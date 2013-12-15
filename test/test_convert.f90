cca3674b72eda24d617939b4c320a4f077f249bd
mastewart <mastewart@gsu.edu>
2013-12-15 20:54:07 +0000

    updated tests to use type(ub) and type(bv)

--- test/test_convert.f90
+++ test/test_convert.f90
@@ -4,37 +4,34 @@ program test_convert
   use assemble
   use upper_convert
   use band
+  use decomp_types
   implicit none
   integer(kind=int32), parameter :: n=500, rmax=4, ubwmax=rmax+1, lbw=3, lbwmax=3, mb=ubwmax+lbwmax+1, nb=mb
   real(kind=dp), parameter :: tol=1e-15
   real(kind=dp) :: t1, t2
-  integer(kind=int32) :: j, k, error
+  integer(kind=int32) :: error
+  character(len=*), parameter :: fmt="(A30, 'Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)"
+  character(len=30) :: test_name
   !
   real(kind=dp), dimension(n,n) :: a, a0, a1
-  real(kind=dp), dimension(mb,n) :: b_ub
-  real(kind=dp), dimension(n,mb) :: b_bv
   real(kind=dp), dimension(n,rmax) :: u, u0
   real(kind=dp), dimension(rmax,n) :: v, v0
   real(kind=dp), dimension(n) :: d
-  real(kind=dp), dimension(mb-lbw-1, n) :: cs_ub, ss_ub
-  real(kind=dp), dimension(n,mb-lbw-1) :: cs_bv, ss_bv
-  integer(kind=int32) :: ubw
-  integer(kind=int32), dimension(mb-lbw-1, n) :: j1s, j2s
-  integer(kind=int32), dimension(n,mb-lbw-1) :: k1s, k2s
-  integer(kind=int32), dimension(n) :: numrots_ub, numrots_bv
-  !
   complex(kind=dp), dimension(n,n) :: a_c, a0_c, a1_c
-  complex(kind=dp), dimension(mb,n) :: b_ub_c
-  complex(kind=dp), dimension(n,mb) :: b_bv_c
   complex(kind=dp), dimension(n,rmax) :: u_c, u0_c
   complex(kind=dp), dimension(rmax,n) :: v_c, v0_c
   complex(kind=dp), dimension(n) :: d_c
-  complex(kind=dp), dimension(mb-lbw-1, n) :: cs_ub_c, ss_ub_c
-  complex(kind=dp), dimension(n,mb-lbw-1) :: cs_bv_c, ss_bv_c
-  integer(kind=int32) :: ubw_c
-  integer(kind=int32), dimension(mb-lbw-1, n) :: j1s_c, j2s_c
-  integer(kind=int32), dimension(n,mb-lbw-1) :: k1s_c, k2s_c
-  integer(kind=int32), dimension(n) :: numrots_ub_c, numrots_bv_c
+
+  type(d_ub) :: ub_d
+  type(c_ub) :: ub_c
+  type(d_bv) :: bv_d
+  type(c_bv) :: bv_c
+  ub_d=d_new_ub(n,lbwmax,ubwmax)
+  ub_c=c_new_ub(n,lbwmax,ubwmax)
+  bv_d=d_new_bv(n,lbwmax,ubwmax)
+  bv_c=c_new_bv(n,lbwmax,ubwmax)
+  ub_d%lbw=lbw;   ub_c%lbw=lbw
+  bv_d%lbw=lbw;   bv_c%lbw=lbw
 
   ! real
   call random_seed
@@ -43,53 +40,39 @@ program test_convert
   call random_number(d)
   u0=u; v0=v
   ! test one
-  a=matmul(u,v)
-  do k=1,n-lbw-1
-     do j=k+lbw+1,n
-        a(j,k)=0.0_dp
-     end do
-  end do
-  do j=1,n
-     a(j,j)=d(j)
-  end do
+  call d_assemble_a(a,u,v,d,lbw)
   a0=a
-  call f_upper_to_ub(a,n, b_ub, lbw, ubw, lbwmax, ubwmax, numrots_ub, j1s, j2s,  &
-       cs_ub, ss_ub, tol, error)
+  call upper_to_ub(a,ub_d,tol,error)
   if (error == 1) then
      print *, "orthogonalization error in real UB"
   else
      call cpu_time(t1)
-     call f_ub_to_bv(b_ub, n, lbw, ubw, lbwmax, ubwmax, numrots_ub, j1s, j2s, cs_ub, ss_ub, &
-          b_bv, lbwmax, ubwmax, numrots_bv, k1s, k2s, cs_bv, ss_bv, error)
+     call ub_to_bv(ub_d, bv_d,error)
      call cpu_time(t2)
      if (error > 0) then
         print *, "Error in upper_ub_to_bv:", error
      else
-        call f_bv_to_upper(b_bv, n, lbw, ubw, lbwmax, ubwmax, numrots_bv, k1s, k2s, cs_bv, ss_bv, a1)
-        write (*, "('Real UB to BV;                     ', 'Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
-             t2-t1, ubw, maxabs(a1-a0)
+        call bv_to_upper(bv_d,a1,error)
+        test_name = "Real UB to BV;"
+        write (*,fmt) test_name, t2-t1, bv_d%ubw, maxabs(a1-a0)
      end if
   end if
 
-
-
   ! bv to ub
   a=a0
-  call f_upper_to_bv(a,n, b_bv, lbw, ubw, lbwmax, ubwmax, numrots_bv, k1s, k2s,  &
-       cs_bv, ss_bv, tol, error)
+  call upper_to_bv(a,bv_d,tol,error)
   if (error == 1) then
      print *, "orthogonalization error in real BV"
   else
      call cpu_time(t1)
-     call f_bv_to_ub(b_bv, n, lbw, ubw, lbwmax, ubwmax, numrots_bv, k1s, k2s, cs_bv, ss_bv, &
-          b_ub, lbwmax, ubwmax, numrots_ub, j1s, j2s, cs_ub, ss_ub, error)
+     call bv_to_ub(bv_d, ub_d, error)
      call cpu_time(t2)
      if (error > 0) then
         print *, "Error in upper_bv_to_ub:", error
      else
-        call f_ub_to_upper(b_ub, n, lbw, ubw, lbwmax, ubwmax, numrots_ub, j1s, j2s, cs_ub, ss_ub, a1)
-        write (*, "('Real BV to UB;                     ', 'Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
-             t2-t1, ubw, maxabs(a1-a0)/maxabs(a0)
+        call ub_to_upper(ub_d,a1,error)
+        test_name="Real BV to UB;"
+        write (*,fmt) test_name, t2-t1, ub_d%ubw, maxabs(a1-a0)/maxabs(a0)
      end if
   end if
 
@@ -99,54 +82,80 @@ program test_convert
   call random_complex(v_c)
   call random_complex(d_c)
   u0_c=u_c; v0_c=v_c
-  a_c=matmul(u_c,v_c)
-  do k=1,n-lbw-1
-     do j=k+lbw+1,n
-        a_c(j,k)=(0.0_dp, 0.0_dp)
-     end do
-  end do
-  do j=1,n
-     a_c(j,j)=d_c(j)
-  end do
-  ! ub to bv
+  call c_assemble_a(a_c, u_c, v_c, d_c, lbw)
   a0_c=a_c
-  call f_upper_to_ub(a_c,n, b_ub_c, lbw, ubw_c, lbwmax, ubwmax, numrots_ub_c, j1s_c, j2s_c,  &
-       cs_ub_c, ss_ub_c, tol, error)
+  call upper_to_ub(a_c, ub_c, tol, error)
   if (error == 1) then
      print *, "orthogonalization error in complex UB"
   else
      call cpu_time(t1)
-     call f_ub_to_bv(b_ub_c, n, lbw, ubw_c, lbwmax, ubwmax, numrots_ub_c, j1s_c, j2s_c, cs_ub_c, ss_ub_c, &
-          b_bv_c, lbwmax, ubwmax, numrots_bv_c, k1s_c, k2s_c, cs_bv_c, ss_bv_c, error)
+     call ub_to_bv(ub_c, bv_c, error)
      call cpu_time(t2)
      if (error > 0) then
         print *, "Error in upper_ub_to_bv:", error
      else
-        call f_bv_to_upper(b_bv_c, n, lbw, ubw_c, lbwmax, ubwmax, numrots_bv_c, &
-             k1s_c, k2s_c, cs_bv_c, ss_bv_c, a1_c)
-        write (*, "('Complex UB to BV;                     ', 'Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
-             t2-t1, ubw_c, maxabs(a1_c-a0_c)/maxabs(a0_c)
+        call bv_to_upper(bv_c, a1_c, error)
+        test_name="Complex UB to BV;"
+        write (*,fmt) test_name, t2-t1, bv_c%ubw, maxabs(a1_c-a0_c)/maxabs(a0_c)
      end if
   end if
   ! bv to ub
   a_c=a0_c
-  call f_upper_to_bv(a_c,n, b_bv_c, lbw, ubw_c, lbwmax, ubwmax, numrots_bv_c, k1s_c, k2s_c,  &
-       cs_bv_c, ss_bv_c, tol, error)
+  call upper_to_bv(a_c, bv_c, tol, error)
   if (error == 1) then
      print *, "orthogonalization error in complex BV"
   else
      call cpu_time(t1)
-     call f_bv_to_ub(b_bv_c, n, lbw, ubw_c, lbwmax, ubwmax, numrots_bv_c, k1s_c, k2s_c, cs_bv_c, ss_bv_c, &
-          b_ub_c, lbwmax, ubwmax, numrots_ub_c, j1s_c, j2s_c, cs_ub_c, ss_ub_c, error)
+     call bv_to_ub(bv_c, ub_c, error)
      call cpu_time(t2)
      if (error > 0) then
         print *, "Error in upper_bv_to_ub:", error
      else
-        call f_ub_to_upper(b_ub_c, n, lbw, ubw_c, lbwmax, ubwmax, numrots_ub_c, &
-             j1s_c, j2s_c, cs_ub_c, ss_ub_c, a1_c)
-        write (*, "('Complex BV to UB;                     ', 'Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
-             t2-t1, ubw_c, maxabs(a1_c-a0_c)/maxabs(a0_c)
+        call ub_to_upper(ub_c, a1_c, error)
+        test_name="Complex BV to UB;"
+        write (*,fmt) test_name, t2-t1, ub_c%ubw, maxabs(a1_c-a0_c)/maxabs(a0_c)
      end if
   end if
+  print *
+
+contains
+  
+  subroutine d_assemble_a(a,u,v,d,lbw)
+    real(kind=dp), dimension(:,:), intent(out) :: a
+    real(kind=dp), dimension(:,:), intent(in) :: u, v
+    real(kind=dp), dimension(:), intent(in) :: d
+    integer(kind=int32), intent(in) :: lbw
+
+    integer(kind=int32) :: j,k,n
+    n=size(a,1)
+    a=matmul(u,v)
+    do k=1,n-lbw-1
+       do j=k+lbw+1,n
+          a(j,k)=0.0_dp
+       end do
+    end do
+    do j=1,n
+       a(j,j)=d(j)
+    end do
+  end subroutine d_assemble_a
+
+  subroutine c_assemble_a(a,u,v,d,lbw)
+    complex(kind=dp), dimension(:,:), intent(out) :: a
+    complex(kind=dp), dimension(:,:), intent(in) :: u, v
+    complex(kind=dp), dimension(:), intent(in) :: d
+    integer(kind=int32), intent(in) :: lbw
+
+    integer(kind=int32) :: j,k,n
+    n=size(a,1)
+    a=matmul(u,v)
+    do k=1,n-lbw-1
+       do j=k+lbw+1,n
+          a(j,k)=(0.0_dp, 0.0_dp)
+       end do
+    end do
+    do j=1,n
+       a(j,j)=d(j)
+    end do
+  end subroutine c_assemble_a
 
 end program test_convert
--- test/test_decomp.f90
+++ test/test_decomp.f90
@@ -2,37 +2,33 @@ program test_decomp
   use upper_decomp
   use utility
   use assemble
+  use decomp_types
   implicit none
-  integer(kind=int32), parameter :: n=1000, rmax=5, ubwmax=rmax, lbw=1, lbwmax=lbw, mb=ubwmax+lbwmax+1, nb=mb
+  integer(kind=int32), parameter :: n=10, rmax=2, ubwmax=rmax, lbw=1, lbwmax=lbw, mb=ubwmax+lbwmax+1, nb=mb
   real(kind=dp), parameter :: tol=1e-14
   real(kind=dp) :: t1, t2
-  integer(kind=int32) :: j, k, error
+  integer(kind=int32) :: error
+  character(len=*), parameter :: fmt="(A30, 'Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)"
+  character(len=30) :: test_name
   !
   real(kind=dp), dimension(n,n) :: a, a0, a1
-  real(kind=dp), dimension(mb,n) :: b_ub
-  real(kind=dp), dimension(n,mb) :: b_bv
   real(kind=dp), dimension(n,rmax) :: u, u0
   real(kind=dp), dimension(rmax,n) :: v, v0
   real(kind=dp), dimension(n) :: d
-  integer(kind=int32) :: ubw
-  integer(kind=int32), dimension(ubwmax, n) :: j1s, j2s
-  integer(kind=int32), dimension(n,ubwmax) :: k1s, k2s
-  integer(kind=int32), dimension(n) :: numrots
-  real(kind=dp), dimension(ubwmax, n) :: cs_ub, ss_ub
-  real(kind=dp), dimension(n,ubwmax) :: cs_bv, ss_bv
-  !
   complex(kind=dp), dimension(n,n) :: ac, a0c, a1c
-  complex(kind=dp), dimension(mb,n) :: bc_ub
-  complex(kind=dp), dimension(n,mb) :: bc_bv
   complex(kind=dp), dimension(n,rmax) :: uc, u0c
   complex(kind=dp), dimension(rmax,n) :: vc, v0c
   complex(kind=dp), dimension(n) :: dc
-  integer(kind=int32) :: ubwc
-  integer(kind=int32), dimension(ubwmax, n) :: j1sc, j2sc
-  integer(kind=int32), dimension(n,ubwmax) :: k1sc, k2sc
-  integer(kind=int32), dimension(n) :: numrotsc
-  complex(kind=dp), dimension(ubwmax, n) :: csc_ub, ssc_ub
-  complex(kind=dp), dimension(n, ubwmax) :: csc_bv, ssc_bv
+  type(d_ub) :: ub_d
+  type(c_ub) :: ub_c
+  type(d_bv) :: bv_d
+  type(c_bv) :: bv_c
+  ub_d=d_new_ub(n,lbwmax,ubwmax)
+  ub_c=c_new_ub(n,lbwmax,ubwmax)
+  bv_d=d_new_bv(n,lbwmax,ubwmax)
+  bv_c=c_new_bv(n,lbwmax,ubwmax)
+  ub_d%lbw=lbw;   ub_c%lbw=lbw
+  bv_d%lbw=lbw;   bv_c%lbw=lbw
 
   call random_seed
   call random_number(u)
@@ -43,170 +39,158 @@ program test_decomp
   call random_complex(vc)
   call random_complex(dc)
   u0c=uc; v0c=vc
+
   ! test one
-  a=matmul(u,v)
-  do k=1,n-lbw-1
-     do j=k+lbw+1,n
-        a(j,k)=0.0_dp
-     end do
-  end do
-  do j=1,n
-     a(j,j)=d(j)
-  end do
+
+  call d_assemble_a(a,u,v,d,lbw)
   a0=a
   call cpu_time(t1)
-  call f_upper_to_ub(a,n, b_ub, lbw, ubw, lbwmax, ubwmax, numrots, j1s, j2s, cs_ub, ss_ub, tol, error)
+  call upper_to_ub(a,ub_d,tol,error)
   call cpu_time(t2)
   if (error == 1) then
      print *, "orthogonalization error in real UB"
   else
-     call f_ub_to_upper(b_ub, n, lbw, ubw, lbwmax, ubwmax, numrots, j1s, j2s, cs_ub, ss_ub, a1)
-     write (*,"('Real UB;                     ', 'Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
-          t2-t1, ubw, maxabs(a1-a0)
+     call ub_to_upper(ub_d,a1,error)
+     test_name="Real UB;"
+     write(*,fmt) test_name, t2-t1, ub_d%ubw, maxabs(a1-a0)
   end if
-  !
+
   ! test 2: square termination.
-  !
+
+  u=u0; v=v0;
   u(1:n-rmax-1,rmax)=0.0_dp
-  a=matmul(u,v)
-  do k=1,n-lbw-1
-     do j=k+lbw+1,n
-        a(j,k)=0.0_dp
-     end do
-  end do
-  do j=1,n
-     a(j,j)=d(j)
-  end do
+  call d_assemble_a(a,u,v,d,lbw)
   a0=a
   call cpu_time(t1)
-  call f_upper_to_ub(a, n, b_ub, lbw, ubw, lbwmax, ubwmax, numrots, j1s, j2s, cs_ub, ss_ub, tol, error)
+  call upper_to_ub(a,ub_d,tol,error)
   call cpu_time(t2)
   if (error == 1) then
      print *, "orthogonalization error in real square termination UB"
   else
-     call f_ub_to_upper(b_ub, n, lbw, ubw, lbwmax, ubwmax, numrots, j1s, j2s, cs_ub, ss_ub, a1)
-     write (*,"('Real Sq. term. UB;           ', 'Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
-          t2-t1, ubw, maxabs(a1-a0)
+     call ub_to_upper(ub_d,a1,error)
+     test_name="Real Sq. term. UB;"
+     write (*,fmt) test_name, t2-t1, ub_d%ubw, maxabs(a1-a0)
   end if
   ! BV
   u=u0; v=v0
-  a=matmul(u,v)
-  do k=1,n-lbw-1
-     do j=k+lbw+1,n
-        a(j,k)=0.0_dp
-     end do
-  end do
-  do j=1,n
-     a(j,j)=d(j)
-  end do
+  call d_assemble_a(a,u,v,d,lbw)
   a0=a
   call cpu_time(t1)
-  call f_upper_to_bv(a, n, b_bv, lbw, ubw, lbwmax, ubwmax, numrots, k1s, k2s, cs_bv, ss_bv, tol, error)
+  call upper_to_bv(a,bv_d,tol,error)
   call cpu_time(t2)
   if (error == 1) then
      print *, "orthogonalization error in real BV"
   else
-     call f_bv_to_upper(b_bv, n, lbw, ubw, lbwmax, ubwmax, numrots, k1s, k2s, cs_bv, ss_bv, a1)
-     write (*,"('Real BV;                     ','Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
-          t2-t1, ubw, maxabs(a1-a0)
+     call bv_to_upper(bv_d,a1,error)
+     test_name="Real BV;"
+     write(*,fmt) test_name, t2-t1, bv_d%ubw, maxabs(a1-a0)
   end if
   ! square termination BV
   u=u0; v=v0
   v(rmax,rmax+2:n)=0.0_dp
-  a=matmul(u,v)
-  do k=1,n-lbw-1
-     do j=k+lbw+1,n
-        a(j,k)=0.0_dp
-     end do
-  end do
-  do j=1,n
-     a(j,j)=d(j)
-  end do
+  call d_assemble_a(a,u,v,d,lbw)
   a0=a
   call cpu_time(t1)
-  call f_upper_to_bv(a,n, b_bv, lbw, ubw, lbwmax, ubwmax, numrots, k1s, k2s, cs_bv, ss_bv, tol, error)
+  call upper_to_bv(a,bv_d,tol,error)
   call cpu_time(t2)
   if (error == 1) then
      print *, "orthogonalization error in real square termination BV"
   else
-     call f_bv_to_upper(b_bv, n, lbw, ubw, lbwmax, ubwmax, numrots, k1s, k2s, cs_bv, ss_bv, a1)
-     write (*,"('Real Sq. Term BV;            ','Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
-          t2-t1, ubw, maxabs(a1-a0)
+     call bv_to_upper(bv_d,a1,error)
+     test_name="Real Sq. Term BV;"
+     write (*,fmt) test_name, t2-t1, bv_d%ubw, maxabs(a1-a0)
   end if
   !
   ! Complex UB test
   !
-  ac=matmul(uc,vc)
-  do k=1,n-lbw-1
-     do j=k+lbw+1,n
-        ac(j,k)=(0.0_dp, 0.0_dp)
-     end do
-  end do
-  do j=1,n
-     ac(j,j)=dc(j)
-  end do
+  uc=u0c; vc=v0c
+  call c_assemble_a(ac,uc,vc,dc,lbw)
   a0c=ac
   call cpu_time(t1)
-  call f_upper_to_ub(ac,n, bc_ub, lbw, ubwc, lbwmax, ubwmax, numrotsc, j1sc, j2sc, &
-       csc_ub, ssc_ub, tol, error)
+  call upper_to_ub(ac,ub_c,tol,error)
   call cpu_time(t2)
   if (error == 1) then
      print *, "orthogonalization error in complex UB"
   else
-     call f_ub_to_upper(bc_ub, n, lbw, ubwc, lbwmax, ubwmax, numrotsc, j1sc, j2sc, csc_ub, ssc_ub, a1c)
-     write (*,"('Complex UB;                  ','Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
-          t2-t1, ubwc, maxabs(a1c-a0c)
+     call ub_to_upper(ub_c,a1c,error)
+     test_name="Complex UB;"
+     write (*,fmt) test_name, t2-t1, ub_c%ubw, maxabs(a1c-a0c)
   end if
   !
   ! Complex UB square termination test
   !
   uc=u0c; vc=v0c
   uc(1:n-rmax-1,rmax)=(0.0_dp, 0.0_dp)
-  ac=matmul(uc,vc)
-  do k=1,n-lbw-1
-     do j=k+lbw+1,n
-        ac(j,k)=(0.0_dp, 0.0_dp)
-     end do
-  end do
-  do j=1,n
-     ac(j,j)=dc(j)
-  end do
+  call c_assemble_a(ac,uc,vc,dc,lbw)
   a0c=ac
   call cpu_time(t1)
-  call f_upper_to_ub(ac,n, bc_ub, lbw, ubwc, lbwmax, ubwmax, numrotsc, j1sc, j2sc, &
-       csc_ub, ssc_ub, tol, error)
+  call upper_to_ub(ac,ub_c,tol,error)
   call cpu_time(t2)
   if (error == 1) then
      print *, "orthogonalization error in complex UB"
   else
-     call f_ub_to_upper(bc_ub, n, lbw, ubwc, lbwmax, ubwmax, numrotsc, j1sc, j2sc, csc_ub, ssc_ub, a1c)
-     write (*,"('Complex square term. UB;     ','Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
-          t2-t1, ubwc, maxabs(a1c-a0c)
+     call ub_to_upper(ub_c,a1c,error)
+     test_name="Complex square term. UB;"
+     write (*,fmt) test_name, t2-t1, ub_c%ubw, maxabs(a1c-a0c)
   end if
   !
   ! Complex BV
   !
   uc=u0c; vc=v0c
   uc(1:n-rmax-1,rmax)=(0.0_dp, 0.0_dp)
-  ac=matmul(uc,vc)
-  do k=1,n-lbw-1
-     do j=k+lbw+1,n
-        ac(j,k)=(0.0_dp, 0.0_dp)
-     end do
-  end do
-  do j=1,n
-     ac(j,j)=dc(j)
-  end do
+  call c_assemble_a(ac,uc,vc,dc,lbw)
   a0c=ac
   call cpu_time(t1)
-  call f_upper_to_bv(ac,n, bc_bv, lbw, ubwc, lbwmax, ubwmax, numrots, k1sc, k2sc, csc_bv, ssc_bv, &
-       tol, error)
+  call upper_to_bv(ac,bv_c,tol,error)
   call cpu_time(t2)
   if (error == 1) then
      print *, "orthogonalization error in complex BV"
   else
-     call f_bv_to_upper(bc_bv, n, lbw, ubwc, lbwmax, ubwmax, numrots, k1sc, k2sc, csc_bv, ssc_bv, a1c)
-     write (*,"('Real BV;                     ','Time: ',ES8.2,', ubw: ',I3,', error: ',ES8.2)") &
-          t2-t1, ubw, maxabs(a1c-a0c)
+     call bv_to_upper(bv_c,a1c,error)
+     test_name="Complex BV;"
+     write (*,fmt) test_name, t2-t1, bv_c%ubw, maxabs(a1c-a0c)
   end if
+  print *
+contains
+  
+  subroutine d_assemble_a(a,u,v,d,lbw)
+    real(kind=dp), dimension(:,:), intent(out) :: a
+    real(kind=dp), dimension(:,:), intent(in) :: u, v
+    real(kind=dp), dimension(:), intent(in) :: d
+    integer(kind=int32), intent(in) :: lbw
+
+    integer(kind=int32) :: j,k,n
+    n=size(a,1)
+    a=matmul(u,v)
+    do k=1,n-lbw-1
+       do j=k+lbw+1,n
+          a(j,k)=0.0_dp
+       end do
+    end do
+    do j=1,n
+       a(j,j)=d(j)
+    end do
+  end subroutine d_assemble_a
+
+  subroutine c_assemble_a(a,u,v,d,lbw)
+    complex(kind=dp), dimension(:,:), intent(out) :: a
+    complex(kind=dp), dimension(:,:), intent(in) :: u, v
+    complex(kind=dp), dimension(:), intent(in) :: d
+    integer(kind=int32), intent(in) :: lbw
+
+    integer(kind=int32) :: j,k,n
+    n=size(a,1)
+    a=matmul(u,v)
+    do k=1,n-lbw-1
+       do j=k+lbw+1,n
+          a(j,k)=(0.0_dp, 0.0_dp)
+       end do
+    end do
+    do j=1,n
+       a(j,j)=d(j)
+    end do
+  end subroutine c_assemble_a
+
+
+
 end program test_decomp
