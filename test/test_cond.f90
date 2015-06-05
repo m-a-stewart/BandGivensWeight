b311b522fe445dd5a6b3a7f1d0d0c20b82b573dc
mastewart <mastewart@gsu.edu>
2015-06-05 18:17:46 +0000

    changed _c to _z in test_cond.f90

--- test/test_cond.f90
+++ test/test_cond.f90
@@ -11,9 +11,9 @@ program test_cond
   real(kind=dp), dimension(:), allocatable :: u_d, v_d, res_d, res_d1
   real(kind=dp) :: sigma_d, tmp
 
-  complex(kind=dp), dimension(:,:), allocatable :: l_c, r_c
-  complex(kind=dp), dimension(:), allocatable :: u_c, v_c, res_c, res_c1
-  real(kind=dp) :: sigma_c
+  complex(kind=dp), dimension(:,:), allocatable :: l_z, r_z
+  complex(kind=dp), dimension(:), allocatable :: u_z, v_z, res_z, res_z1
+  real(kind=dp) :: sigma_z
   
   print *
   print *, "--------------------------------"
@@ -106,83 +106,83 @@ program test_cond
   print *, "Complex Condition Estimation Tests"
   print *
   n=30
-  allocate(l_c(n,n),u_c(n),v_c(n),res_c(n),res_c1(n))
-  call random_matrix_to(l_c)
+  allocate(l_z(n,n),u_z(n),v_z(n),res_z(n),res_z1(n))
+  call random_matrix_to(l_z)
   do j=1,n
      do k=j+1,n
-        l_c(j,k)=(0.0_dp,0.0_dp)
+        l_z(j,k)=(0.0_dp,0.0_dp)
      end do
   end do
 
-  sigma_c=lower_min_sv(l_c,u_c,v_c,res_c,1.0e-14_dp,1000,error)
+  sigma_z=lower_min_sv(l_z,u_z,v_z,res_z,1.0e-14_dp,1000,error)
 
-  tmp=norm2(res_c)
-  call lower_left_multiply(l_c,v_c,res_c)
-  res_c=res_c-sigma_c*u_c
-  call lower_tr_left_multiply(l_c,u_c,res_c1)
-  res_c1=res_c1-sigma_c*v_c
+  tmp=norm2(res_z)
+  call lower_left_multiply(l_z,v_z,res_z)
+  res_z=res_z-sigma_z*u_z
+  call lower_tr_left_multiply(l_z,u_z,res_z1)
+  res_z1=res_z1-sigma_z*v_z
   test_name='Lower Min SV'
-  call output_result(test_name,sigma_c,tmp,norm2(res_c),norm2(res_c1),tol1,error)
+  call output_result(test_name,sigma_z,tmp,norm2(res_z),norm2(res_z1),tol1,error)
 
-  sigma_c=lower_max_sv(l_c,u_c,v_c,res_c,tol,1000,error)
-  tmp=norm2(res_c)
-  call lower_left_multiply(l_c,v_c,res_c)
-  res_c=res_c-sigma_c*u_c
-  call lower_tr_left_multiply(l_c,u_c,res_c1)
-  res_c1=res_c1-sigma_c*v_c
+  sigma_z=lower_max_sv(l_z,u_z,v_z,res_z,tol,1000,error)
+  tmp=norm2(res_z)
+  call lower_left_multiply(l_z,v_z,res_z)
+  res_z=res_z-sigma_z*u_z
+  call lower_tr_left_multiply(l_z,u_z,res_z1)
+  res_z1=res_z1-sigma_z*v_z
   test_name='Lower Max SV'
-  call output_result(test_name,sigma_c,tmp,norm2(res_c),norm2(res_c1),tol1,error)
+  call output_result(test_name,sigma_z,tmp,norm2(res_z),norm2(res_z1),tol1,error)
 
-  l_c(5,5)=(2.3e-40_dp,5e-40_dp)
-  l_c(10,10)=(2.3e-40_dp,5e-40_dp)
-  sigma_c=lower_min_sv(l_c,u_c,v_c,res_c,1.0e-14_dp,1000,error)
-  tmp=norm2(res_c)
-  call lower_left_multiply(l_c,v_c,res_c)
-  res_c=res_c-sigma_c*u_c
-  call lower_tr_left_multiply(l_c,u_c,res_c1)
-  res_c1=res_c1-sigma_c*v_c
+  l_z(5,5)=(2.3e-40_dp,5e-40_dp)
+  l_z(10,10)=(2.3e-40_dp,5e-40_dp)
+  sigma_z=lower_min_sv(l_z,u_z,v_z,res_z,1.0e-14_dp,1000,error)
+  tmp=norm2(res_z)
+  call lower_left_multiply(l_z,v_z,res_z)
+  res_z=res_z-sigma_z*u_z
+  call lower_tr_left_multiply(l_z,u_z,res_z1)
+  res_z1=res_z1-sigma_z*v_z
   test_name='Lower Min SV'
-  call output_result(test_name,sigma_c,tmp,norm2(res_c),norm2(res_c1),tol1,error)
-  deallocate(l_c,u_c,v_c,res_c,res_c1)
+  call output_result(test_name,sigma_z,tmp,norm2(res_z),norm2(res_z1),tol1,error)
+  deallocate(l_z,u_z,v_z,res_z,res_z1)
   
   !upper
   n=20
-  allocate(r_c(n,n),u_c(n),v_c(n),res_c(n),res_c1(n))
-  call random_matrix_to(r_c)
+  allocate(r_z(n,n),u_z(n),v_z(n),res_z(n),res_z1(n))
+  call random_matrix_to(r_z)
   do j=1,n
      do k=1,j-1
-        r_c(j,k)=0.0_dp
+        r_z(j,k)=0.0_dp
      end do
   end do
-  sigma_c=upper_min_sv(r_c,u_c,v_c,res_c,tol,1000,error)
-  tmp=norm2(res_c)
-  call upper_left_multiply(r_c,v_c,res_c)
-  res_c=res_c-sigma_c*u_c
-  call upper_tr_left_multiply(r_c,u_c,res_c1)
-  res_c1=res_c1-sigma_c*v_c
+  sigma_z=upper_min_sv(r_z,u_z,v_z,res_z,tol,1000,error)
+  tmp=norm2(res_z)
+  call upper_left_multiply(r_z,v_z,res_z)
+  res_z=res_z-sigma_z*u_z
+  call upper_tr_left_multiply(r_z,u_z,res_z1)
+  res_z1=res_z1-sigma_z*v_z
   test_name='Upper Min SV'
-  call output_result(test_name,sigma_c,tmp,norm2(res_c),norm2(res_c1),tol1,error)
+  call output_result(test_name,sigma_z,tmp,norm2(res_z),norm2(res_z1),tol1,error)
 
-  sigma_c=upper_max_sv(r_c,u_c,v_c,res_c,tol,1000,error)
-  tmp=norm2(res_c)
-  call upper_left_multiply(r_c,v_c,res_c)
-  res_c=res_c-sigma_c*u_c
-  call upper_tr_left_multiply(r_c,u_c,res_c1)
-  res_c1=res_c1-sigma_c*v_c
+  sigma_z=upper_max_sv(r_z,u_z,v_z,res_z,tol,1000,error)
+  tmp=norm2(res_z)
+  call upper_left_multiply(r_z,v_z,res_z)
+  res_z=res_z-sigma_z*u_z
+  call upper_tr_left_multiply(r_z,u_z,res_z1)
+  res_z1=res_z1-sigma_z*v_z
   test_name='Upper Max SV'
-  call output_result(test_name,sigma_c,tmp,norm2(res_c),norm2(res_c1),tol1,error)
+  call output_result(test_name,sigma_z,tmp,norm2(res_z),norm2(res_z1),tol1,error)
 
-  r_c(5,5)=(2.3e-40_dp,5e-40_dp)
-  r_c(10,10)=(2.3e-40_dp,5e-40_dp)
-  sigma_c=upper_min_sv(r_c,u_c,v_c,res_c,tol,1000,error)
-  tmp=norm2(res_c)
-  call upper_left_multiply(r_c,v_c,res_c)
-  res_c=res_c-sigma_c*u_c
-  call upper_tr_left_multiply(r_c,u_c,res_c1)
-  res_c1=res_c1-sigma_c*v_c
+  r_z(5,5)=(2.3e-40_dp,5e-40_dp)
+  r_z(10,10)=(2.3e-40_dp,5e-40_dp)
+  sigma_z=upper_min_sv(r_z,u_z,v_z,res_z,tol,1000,error)
+  tmp=norm2(res_z)
+  call upper_left_multiply(r_z,v_z,res_z)
+  res_z=res_z-sigma_z*u_z
+  call upper_tr_left_multiply(r_z,u_z,res_z1)
+  res_z1=res_z1-sigma_z*v_z
   test_name='Upper Min SV'
-  call output_result(test_name,sigma_c,tmp,norm2(res_c),norm2(res_c1),tol1,error)
-  deallocate(r_c,u_c,v_c,res_c,res_c1)
+  call output_result(test_name,sigma_z,tmp,norm2(res_z),norm2(res_z1),tol1,error)
+  deallocate(r_z,u_z,v_z,res_z,res_z1)
   print *
 
 contains
